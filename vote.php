9067fb38b55121f237081d9b8036c5e40ca4d0e5
Marrr5 <maryangegz@gmail.com>
2026-04-30 18:20:46 +0000

    first

---
+++ vote.php
@@ -0,0 +1,22 @@
+<?php
+$candidat = $argv[1];
+$telephone = $argv[2];
+
+$conn = new mysqli('localhost', 'debian-sys-maint', 'NoHKV3P9XUAUZOqt', 'votes_lutte');
+
+if ($conn->connect_error) {
+    error_log("Connexion echouee: " . $conn->connect_error);
+    exit(1);
+}
+
+$stmt = $conn->prepare("INSERT INTO votes (candidat, telephone) VALUES (?, ?)");
+if (!$stmt) {
+    error_log("Erreur preparation: " . $conn->error);
+    exit(1);
+}
+
+$stmt->bind_param("ss", $candidat, $telephone);
+$stmt->execute();
+$stmt->close();
+$conn->close();
+?>
